1f627e98b60f1b1972cca42a9205d7f03c3d7582
Grigorii Lutkov <friend.lga@gmail.com>
2022-04-21 21:03:23 +0300

    Remove tests

--- test/controllers/advertiser_controller_test.rb
+++
@@ -1,8 +0,0 @@
-require "test_helper"
-
-class AdvertiserControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get advertiser_index_url
-    assert_response :success
-  end
-end
